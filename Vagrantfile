b19bdb079f5d8d2644e13f5d0da63a08b9c65a0b
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-02-09 16:40:54 +0100

    not rdy, but implemented logic for automatic mounting

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,6 @@ vars 			= YAML.load_file('group_vars/all/config.yml')
 ip_first_part 	= vars['ip_first_part']
 NAME 			= vars["name"]
 
-
 vars["user"].each do |index, ip|
 	if ip < 0 || ip > 22
 		puts "ERROR: User number is out of range. The allowed range is from 0 to inclusive 22."
@@ -24,24 +23,29 @@ vars["user"].each do |index, ip|
 	end
 end
 
-vars["machines"].each do |index|
-	if index['ip'] < 2 || index['ip'] >= 20
-		puts "ERROR: Index for your machines are out of range. The allowed range is from 2 to inclusive 19."
-		exit
-	end
+unless vars["machines"].nil?
+	vars["machines"].each do |index|
+		if index['ip'] < 2 || index['ip'] >= 20
+			puts "ERROR: Index for your machines are out of range. The allowed range is from 2 to inclusive 19."
+			exit
+		end
 
-	if index['ip'] >= 10 && index['ip'] < 20
-		index['ip'] = index['ip'] - 10
-		ip_last_part[NAME] = ip_last_part[NAME].to_i + 1
-	end
-	machine = NAME + "-" + index['name']
-	nodes.push( { :hostname => machine, :ip => ip_first_part + "." + ip_last_part[NAME].to_s + "#{index['ip']}"} )
+		if index['ip'] >= 10 && index['ip'] < 20
+			index['ip'] = index['ip'] - 10
+			ip_last_part[NAME] = ip_last_part[NAME].to_i + 1
+		end
+		machine = NAME + "-" + index['name']
+		nodes.push( { :hostname => machine, :ip => ip_first_part + "." + ip_last_part[NAME].to_s + "#{index['ip']}"} )
 
-	unless index['groups'].nil?
-		index['groups'].each do |g|
-			server_groups[g].push machine
+		unless index['groups'].nil?
+			index['groups'].each do |g|
+				server_groups[g].push machine
+			end
 		end
 	end
+else
+	puts "Please enter a machine name in the config file!"
+	exit
 end
 
 # Only use one mail server per subnet
@@ -50,14 +54,17 @@ if vars["mailserver"] == true
 end
 
 Vagrant.configure("2") do |config|
+	File.delete("ip.txt");
 	config.vm.box = "debian/jessie64"
 
 	# do for each virtual machine
 	nodes.each do |node|
+
+		File.open("ip.txt", "a") { |f| f.write(node[:hostname] + " " + node[:ip] + "\n") }
 		config.vm.define node[:hostname] do |nodeconfig|
-			puts node[:ip]
+			
 			nodeconfig.vm.hostname = node[:hostname]
-			nodeconfig.vm.network :public_network, ip: node[:ip]
+			nodeconfig.vm.network :private_network, ip: node[:ip]
 			nodeconfig.vm.provider "virtualbox" do |vb|
 				vb.memory = 512
 				vb.name = node[:hostname]
--- vag.py
+++ vag.py
@@ -4,6 +4,14 @@
 import yaml
 import sys
 import os
+# from argparse import ArgumentParser
+
+# parser = ArgumentParser(description = "Starts Vagrant")
+# parser.add_argument("-m", "--mount", dest="mountvm", action="store_true")
+# parser.add_argument("command", metavar="commands", nargs="?", help="commnds piped to vagrant i.e. up/halt/destroy")
+# args = parser.parse_args()
+
+
 
 if not os.path.isfile("/usr/local/bin/vag"):
 	src 	= os.getcwd() + "/vag.py"
@@ -20,9 +28,12 @@ if os.geteuid() == 0:
 # generate a dynamic playbook file
 with open("group_vars/all/config.yml", "r") as stream:
 	try:
-		server_roles = yaml.load(stream)['server_roles']
+		config = yaml.load(stream)
 	except yaml.YAMLError as exc:
 		print(exc)
+
+server_roles = config['server_roles']
+password = config['master_pass']
 
 stream	= open("playbook.yml", "w")
 stream.write("---\n")
@@ -42,10 +53,75 @@ for server_role in server_roles:
 
 stream.close()
 
+def getLines():
+	with open("ip.txt") as f:
+		return f.readlines()
 
 # get command args without the first (program name)
 args 	= sys.argv[1:]
 cmd 	= "vagrant " +  " ".join(args)
-# call vagrant with command args
 os.system(cmd)
+
+home_path = os.getenv("HOME")
+
+if "up" in args:
+	mkdir = "mkdir -p %s/mount" % (home_path)
+	os.system(mkdir)
+
+	if len(args) > 1:
+		for arg in args[1:]:
+			if arg == "mailer":
+				continue
+			for line in getLines():
+				l = line.rstrip('\n')
+				ip.append(l[l.find(' '):].strip())
+			ddir = "mkdir -p %s/mount/%s" % (home_path, arg)
+			cmd = "echo %s | sshfs -o password_stdin root@%s:/home/vagrant %s/mount/%s" % (password, ip, home_path, arg)
+			print cmd + " arg"
+			os.system(cmd)
+	else:
+		for line in getLines():
+			l = line.rstrip('\n')
+			name = l[:l.find(' ')].strip()
+			if name == "mailer":
+				continue
+			ip = l[l.find(' '):].strip()
+			ddir = "mkdir -p %s/mount/%s" % (home_path, name)
+			os.system(ddir)
+			cmd = "echo %s | sshfs -o password_stdin root@%s:/home/vagrant %s/mount/%s" % (password, ip, home_path, name)
+			print cmd
+			os.system(cmd)
+
+if ("halt" in args) or ("destroy" in args):
+	if len(args) > 1:
+		for arg in args[1:]:
+			if arg == "mailer":
+				continue
+			cmd = "umount -f %s/mount/%s" % (home_path, arg)
+			print cmd
+			os.system(cmd)
+	else:
+		for line in getLines():
+			l = line.rstrip('\n')
+			name = l[:l.find(' ')].strip()
+			if name == "mailer":
+				continue
+			cmd = "umount -f %s/mount/%s" % (home_path, name)
+			print cmd
+			os.system(cmd)
+
+	if not os.listdir("%s/mount/" % (home_path)):
+		print "in listdir"
+		cmd = "rm -rf %s/mount" % (home_path)
+		os.system(cmd)
+
+
+
+
+
+
+
+
+
+
 
